049b7e8f8ff1a38b2a53c13bb09b584cd5b963bb
Ahmed Elkoussy <aaelkoussy@gmail.com>
2020-06-06 21:55:16 +0200

    added specs for nps controller

--- spec/requests/net_promoter_scores_request_spec.rb
+++ spec/requests/net_promoter_scores_request_spec.rb
@@ -1,5 +1,58 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'jwt'
 
-RSpec.describe "NetPromoterScores", type: :request do
+payload = {
+  touchpoint: 'realtor_feedback',
+  respondent_class: 'seller',
+  respondent_id: '1523',
+  rated_object_class: 'realtor',
+  rated_object_id: '421'
+}
 
+token = JWT.encode payload, ENV['SECRET_KEY'], 'HS512'
+
+RSpec.describe 'add a NPS:', type: :request do
+  it 'throw exception if param is missing' do
+    post '/net_promoter_scores', params: {
+      score: 9
+      # token: token
+    }
+    expect(response).to have_http_status(:bad_request)
+  end
+
+  it 'returns a created status' do
+    post '/net_promoter_scores', params: {
+      score: 9,
+      token: token
+    }
+    expect(response).to have_http_status(:created)
+  end
+end
+
+RSpec.describe 'add a NPS that was created before (shall be update instead of create', type: :request do
+  before do
+    post '/net_promoter_scores', params: {
+      score: 5,
+      token: token
+    }
+  end
+
+  it 'returns an ok status' do
+    expect(response).to have_http_status(:created)
+  end
+end
+
+RSpec.describe 'update a NPS: ', type: :request do
+  before do
+    put '/net_promoter_scores/1', params: {
+      score: 7,
+      token: token
+    }
+  end
+
+  it 'returns an ok status' do
+    expect(response).to have_http_status(:ok)
+  end
 end
