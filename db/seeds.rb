c76b20a9bf8d4272cbbf1200a3212de0b1ba6e9e
Ahmed Elkoussy <aaelkoussy@gmail.com>
2020-06-07 00:33:36 +0200

    added some seed data for the NPS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+NetPromoterScore.create(score: 9, respondent_id: '1523', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421')
+NetPromoterScore.create(score: 10, respondent_id: '2412', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421')
+NetPromoterScore.create(score: 3, respondent_id: '2415', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421')
+NetPromoterScore.create(score: 8, respondent_id: '2444', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421')
+NetPromoterScore.create(score: 9, respondent_id: '2745', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421')
+NetPromoterScore.create(score: 9, respondent_id: '6234', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421')
+NetPromoterScore.create(score: 9, respondent_id: '4213', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421')
+NetPromoterScore.create(score: 9, respondent_id: '15123', touchpoint: 'realtor_feedback', respondent_class: 'buyer', rated_object_class: 'realtor', rated_object_id: '421')
+NetPromoterScore.create(score: 9, respondent_id: '12512', touchpoint: 'realtor_feedback', respondent_class: 'buyer', rated_object_class: 'realtor', rated_object_id: '421')
