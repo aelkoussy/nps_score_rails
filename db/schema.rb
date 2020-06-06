a285994f1a2b4e805bd91a9fac300d8d8df53a89
Ahmed Elkoussy <aaelkoussy@gmail.com>
2020-06-06 14:25:29 +0200

    use postgres instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_06_115527) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "net_promoter_scores", force: :cascade do |t|
     t.integer "score"
     t.string "touchpoint"
