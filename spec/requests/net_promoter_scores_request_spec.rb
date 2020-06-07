567d67a667276b26dfaa1c658c0c43e7aabf2f02
Ahmed Elkoussy <aaelkoussy@gmail.com>
2020-06-08 01:32:38 +0200

    removed update in controller specs, added test data and tested specific output for the index method with diff params

--- spec/requests/net_promoter_scores_request_spec.rb
+++ spec/requests/net_promoter_scores_request_spec.rb
@@ -12,6 +12,18 @@ token = NetPromoterScore.generate_token(
   '421'
 )
 
+array_of_nps = [{ score: 9, respondent_id: '1523', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421' },
+                { score: 10, respondent_id: '2412', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421' },
+                { score: 3, respondent_id: '2415', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421' },
+                { score: 8, respondent_id: '2444', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421' },
+                { score: 9, respondent_id: '2745', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421' },
+                { score: 9, respondent_id: '6234', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421' },
+                { score: 9, respondent_id: '4213', touchpoint: 'realtor_feedback', respondent_class: 'seller', rated_object_class: 'realtor', rated_object_id: '421' },
+                { score: 9, respondent_id: '15123', touchpoint: 'realtor_feedback', respondent_class: 'buyer', rated_object_class: 'realtor', rated_object_id: '421' },
+                { score: 9, respondent_id: '12512', touchpoint: 'realtor_feedback', respondent_class: 'buyer', rated_object_class: 'realtor', rated_object_id: '421' }]
+
+NetPromoterScore.create(array_of_nps)
+
 RSpec.describe 'add a NPS:', type: :request do
   it 'throw exception if param is missing' do
     post '/net_promoter_scores', params: {
@@ -42,34 +54,31 @@ RSpec.describe 'add a NPS that was created before (shall update the record not c
   end
 end
 
-RSpec.describe 'update a NPS: ', type: :request do
-  before do
-    put '/net_promoter_scores/1', params: {
-      score: 7,
-      token: token
-    }
-  end
-
-  it 'returns an ok status if the record is updated' do
-    expect(response).to have_http_status(:ok)
-  end
-end
-
 RSpec.describe 'Get NPS with given params: ', type: :request do
-  it 'returns an ok status if touchpoint is given' do
+  it 'returns an ok status if touchpoint is given and returns a correct number' do
     get '/net_promoter_scores/', params: {
       touchpoint: 'realtor_feedback'
     }
     expect(response).to have_http_status(:ok)
+    expect(response.body).to eq('66.67')
   end
 
-  it 'returns an ok status if all params are given' do
+  it 'returns an ok status if touchpoint & respondent_class are given and returns a correct number' do
+    get '/net_promoter_scores/', params: {
+      touchpoint: 'realtor_feedback',
+      respondent_class: 'buyer'
+    }
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to eq('100.0')
+  end
+  it 'returns an ok status if all params are given and returns a correct number' do
     get '/net_promoter_scores/', params: {
       touchpoint: 'realtor_feedback',
       respondent_class: 'seller',
       rated_object_class: 'realtor'
     }
     expect(response).to have_http_status(:ok)
+    expect(response.body).to eq('57.14')
   end
   it 'returns bad_request status if touchpoint is missing' do
     get '/net_promoter_scores/'
